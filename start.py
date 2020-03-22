6c01c81dd82c1b00aeb89c917ada17c0cae35be2
Pranjal705 <62437656+Pranjal705@users.noreply.github.com>
2020-03-22 20:46:12 +0530

    Age_in_seconds

--- start.py
+++ start.py
@@ -28,3 +28,29 @@ if list1!=[]:
     print('Bad word')
 else:
     print('Good word')
+    
+    #Age in Seconds
+#Houseflies have an approximate life of four weeks. Given the number of days a
+#housefly lived, design an algorithm and write the Python code to determine its
+#approximate age in seconds. Check for boundary conditions and print ‘Invalid
+#input’ if condition is not satisfied. For example, if a housefly lived for 21
+#days then its approximate age in seconds is 21*24*60*60 is 1814400.
+
+#Input Format
+
+#Number of days, n
+
+#Output Format
+
+#Number of seconds
+
+#Boundary Conditions
+
+#n>0 and n <28
+
+n=int(input('enter the no. of days'))
+if n<0 or n>28:
+    print('Invalid input')
+else:
+    days=n*24*60*60
+    print(days)
